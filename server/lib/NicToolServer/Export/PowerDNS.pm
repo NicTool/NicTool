9ccf897db1f02d659c5797d1576f1f0984a1a1a2
Matt Simerson <msimerson@cpan.org>
2012-02-12 13:49:53 -0800

    doc updates

--- server/lib/NicToolServer/Export/PowerDNS.pm
+++ server/lib/NicToolServer/Export/PowerDNS.pm
@@ -1,6 +1,20 @@
 package NicToolServer::Export::PowerDNS;
 # ABSTRACT: exporting DNS data to PowerDNS servers
 
+=pod
+
+A working PowerDNS pipe backend script is included in the NicToolServer 
+distribution as bin/nt_powerdns.pl. It serves PowerDNS requests directly
+from the data in the NicTool tables.
+
+This module is primarily for documentation. It could be used to populate 
+a set of PowerDNS tables in the native PowerDNS SQL schema. If it were 
+used that way, each of the zr_ methods below would contain a query used 
+to insert the records. Search the NicTool mailing list archives for 'PowerDNS'
+for more info.
+
+=cut
+
 use strict;
 use warnings;
 
@@ -61,96 +75,53 @@ sub get_records {
 sub postflight {
     my $self = shift;
 
-# TODO: 
+# TODO
 
     return 1;
 }
 
 sub zr_a {
     my ($self, $r) = @_;
-
-# name  ttl  class  type  type-specific-data
-    return "$r->{name}	$r->{ttl}	A	$r->{address}\n";
 }
 
 sub zr_cname {
     my ($self, $r) = @_;
-
-# name  ttl  class   rr     canonical name
-    return "$r->{name}	$r->{ttl}	CNAME	$r->{address}\n";
 }
 
 sub zr_mx {
     my ($self, $r) = @_;
-
-#name           ttl  class   rr  pref name
-    return "$r->{name}	$r->{ttl}	MX	$r->{weight}	$r->{address}\n";
 }
 
 sub zr_txt {
     my ($self, $r) = @_;
-
-# name  ttl  class   rr     text
-    return "$r->{name}	$r->{ttl}	TXT	\"$r->{address}\"\n";
 }
 
 sub zr_ns {
     my ($self, $r) = @_;
-
-# name  ttl  class  type  type-specific-data
-    return "$r->{name}.	$r->{ttl}	NS	$r->{address}\n";
 }
 
 sub zr_ptr {
     my ($self, $r) = @_;
-
-# name  ttl  class  type  type-specific-data
-    return "$r->{name}	$r->{ttl}	PTR	$r->{address}\n";
 }
 
 sub zr_soa {
     my ($self, $z) = @_;
-
-# name        ttl class rr    name-server email-addr  (sn ref ret ex min)
-    return "$z->{zone}.		$z->{ttl}	IN	SOA	$z->{nsname}    $z->{mailaddr} (
-					$z->{serial}    ; serial
-					$z->{refresh}   ; refresh
-					$z->{retry}     ; retry
-					$z->{expire}    ; expiry
-					$z->{minimum}   ; minimum
-					)\n\n";
 }
 
 sub zr_spf {
     my ($self, $r) = @_;
-
-# SPF record support was added in BIND v9.4.0
-
-# name  ttl  class  type  type-specific-data
-    return "$r->{name}	$r->{ttl}	SPF	$r->{address}\n";
 }
 
 sub zr_srv {
     my ($self, $r) = @_;
-
-    my $priority = $self->{nte}->is_ip_port( $r->{priority} );
-    my $weight   = $self->{nte}->is_ip_port( $r->{weight} );
-    my $port     = $self->{nte}->is_ip_port( $r->{other} );
-
-# srvce.prot.name  ttl  class   rr  pri  weight port target
-    return "$r->{name}	$r->{ttl}	SRV	$priority	$weight	$port	$r->{address}\n";
 }
 
 sub zr_aaaa {
     my ($self, $r) = @_;
-
-# name  ttl  class  type  type-specific-data
-    return "$r->{name}	$r->{ttl}	AAAA	$r->{address}\n";
 }
 
 sub zr_loc {
     my ($self, $r) = @_;
-    return "$r->{name}	$r->{ttl}	LOC	$r->{address}\n";
 }
 
 
