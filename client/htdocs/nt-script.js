44985aa2b9a08bcdb83002385a098b8009d4f7b3
Matt Simerson <msimerson@cpan.org>
2013-04-01 23:13:22 -0500

    nt-script: reset RR vals when switching RR type

--- client/htdocs/nt-script.js
+++ client/htdocs/nt-script.js
@@ -87,7 +87,7 @@ function resetZoneRecordFormFields() {
   for ( var i=0; i < rrFields.length; i++ ) {
     $('tr#tr_' + rrFields[i] ).hide();
     $('td#'+rrFields[i] +'_label').text( ucfirst( rrFields[i] ) );
-    $('input#'+rrFields[i] ).attr('readonly', false);
+    $('input#'+rrFields[i] ).val('').attr('readonly', false);
   };
 };
 
