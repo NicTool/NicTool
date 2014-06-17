6e04d7a5f83230183c417860cdb4b8918ee4becb
Matt Simerson <matt@tnpi.net>
2014-06-16 18:09:45 -0700

    powerdns: default export class for BIND backend

--- server/lib/NicToolServer/Export/PowerDNS.pm
+++ server/lib/NicToolServer/Export/PowerDNS.pm
@@ -3,130 +3,80 @@ package NicToolServer::Export::PowerDNS;
 
 =pod
 
-A working PowerDNS pipe backend script is included in the NicToolServer 
+A working PowerDNS pipe backend script is included in the NicToolServer
 distribution as bin/nt_powerdns.pl. It serves PowerDNS requests directly
 from the data in the NicTool tables.
 
-This module is primarily for documentation. It could be used to populate 
-a set of PowerDNS tables in the native PowerDNS SQL schema. If it were 
-used that way, each of the zr_ methods below would contain a query used 
-to insert the records. Search the NicTool mailing list archives for 'PowerDNS'
-for more info.
+This module is used for PowerDNS with the zone files back end.
 
 =cut
 
 use strict;
 use warnings;
 
+use parent 'NicToolServer::Export::BIND';
+
 use Cwd;
+use IO::File;
 use File::Copy;
-use Params::Validate qw/ :all /;
 
-sub new {
-    my $class = shift;
-
-    my $self = bless {
-        nte => shift,
-        zone_list => [],
-    },
-    $class;
-
-    warn "oops, a NicToolServer::Export object wasn't provided!" 
-        if ! $self->{nte};
-    return $self;
-}
-
-sub export_db {
+sub write_makefile {
     my $self = shift;
 
-    foreach my $z ( @{ $self->{nte}->get_ns_zones() } ) {
-        push @{$self->{zone_list}}, $z->{zone};
-# ....
-        $self->{nte}{zone_name} = $z->{zone};
-#        print $fh $self->{nte}->zr_soa( $z );
-#        print $fh $self->{nte}->zr_ns( $z );
+    my $exportdir = $self->{nte}->get_export_dir or do {
+        warn "no export dir!";
+        return;
+    };
+    return 1 if -e "$exportdir/Makefile";   # already exists
 
-        my $records = $self->get_records( $z->{nt_zone_id} );
-        foreach my $r ( @$records ) {
-            my $type   = lc( $r->{type} );
-            my $method = "zr_${type}";
-            $r->{location}  ||= '';
-# each of the zone record methods below need to be altered to return whatever
-# PowerDNS needs
-#            print $fh $self->$method( $r );
-        }
+    my $address = $self->{nte}{ns_ref}{address} || '127.0.0.1';
+    my $datadir = $self->{nte}{ns_ref}{datadir} || getcwd . '/data-all';
+    $datadir =~ s/\/$//;  # strip off any trailing /
+    open my $M, '>', "$exportdir/Makefile" or do {
+        warn "unable to open ./Makefile: $!\n";
+        return;
+    };
+    print $M <<MAKE
+# After a successful export, 3 make targets are run: compile, remote, restart
+# Each target can do anything you'd like.
+# See https://www.gnu.org/software/make/manual/make.html
 
-    }   
-};
+################################
+#########  PowerDNS  ###########
+################################
 
-sub get_records {
-    my $self = shift;
-    my $zone_id = shift;
+compile: $exportdir/named.conf.nictool
+\ttest 1
 
-    my $sql = "SELECT r.name, r.ttl, r.description, t.name AS type, r.address, r.weight, 
-    priority, other, location, UNIX_TIMESTAMP(timestamp) AS timestamp
-        FROM nt_zone_record r
-        LEFT JOIN resource_record_type t ON t.id=r.type_id
-         WHERE r.deleted=0 AND r.nt_zone_id=?";
+remote: $exportdir/named.conf.nictool
+\trsync -az --delete $exportdir/ powerdns\@$address:$datadir/
 
-    return $self->{nte}->exec_query( $sql, $zone_id );
-}
-
-sub postflight {
-    my $self = shift;
-
-# TODO
-
+restart: $exportdir/named.conf.nictool
+\tssh powerdns\@$address pdns_control cycle
+MAKE
+;
+    close $M;
     return 1;
-}
-
-sub zr_a {
-    my ($self, $r) = @_;
-}
-
-sub zr_cname {
-    my ($self, $r) = @_;
-}
-
-sub zr_mx {
-    my ($self, $r) = @_;
-}
-
-sub zr_txt {
-    my ($self, $r) = @_;
-}
-
-sub zr_ns {
-    my ($self, $r) = @_;
-}
-
-sub zr_ptr {
-    my ($self, $r) = @_;
-}
-
-sub zr_soa {
-    my ($self, $z) = @_;
-}
-
-sub zr_spf {
-    my ($self, $r) = @_;
-}
-
-sub zr_srv {
-    my ($self, $r) = @_;
-}
-
-sub zr_aaaa {
-    my ($self, $r) = @_;
-}
-
-sub zr_loc {
-    my ($self, $r) = @_;
-}
-
+};
 
 1;
 
 __END__
 
+=head1 NAME
 
+NicToolServer::Export::PowerDNS
+
+=head1 SYNOPSIS
+
+Export DNS information from to PowerDNS BIND file backend
+
+=head1 named.conf.local
+
+This class will export a named.conf.nictool file with all the zones assigned to that nameserver.
+
+=head1 AUTHOR
+
+Matt Simerson
+
+=cut
