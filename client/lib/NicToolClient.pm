37af620658bd0d0fe96e6062a057f372e6b99317
Matt Simerson <matt@tnpi.net>
2014-07-08 21:35:55 -0700

    version bump

--- client/lib/NicToolClient.pm
+++ client/lib/NicToolClient.pm
@@ -5,7 +5,7 @@ use strict;
 use vars qw/ $AUTOLOAD /;
 use NicToolServerAPI();
 
-$NicToolClient::VERSION = '2.26';
+$NicToolClient::VERSION = '2.27';
 $NicToolClient::NTURL   = 'http://www.nictool.com/';
 $NicToolClient::LICENSE = 'http://www.affero.org/oagpl.html';
 $NicToolClient::SRCURL  = 'http://www.nictool.com/download/NicTool.tar.gz';
