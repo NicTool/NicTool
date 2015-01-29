4c47017f1e0c126e1b737e8caabed6edc26feff7
Matt Simerson <msimerson@cpan.org>
2015-01-28 18:23:05 -0600

    version bump to 2.31

--- client/lib/NicToolClient.pm
+++ client/lib/NicToolClient.pm
@@ -5,7 +5,7 @@ use strict;
 use vars qw/ $AUTOLOAD /;
 use NicToolServerAPI();
 
-$NicToolClient::VERSION = '2.30';
+$NicToolClient::VERSION = '2.31';
 $NicToolClient::NTURL   = 'http://www.nictool.com/';
 $NicToolClient::LICENSE = 'http://www.affero.org/oagpl.html';
 $NicToolClient::SRCURL  = 'http://www.nictool.com/download/NicTool.tar.gz';
