6e11abb1a379f171e853cd822edbb4aa3f4baa1c
Matt Simerson <matt@tnpi.net>
2017-02-17 14:21:45 -0800

    sort usable NS, to avoid intermittent test failure

--- server/lib/NicToolServer/Nameserver.pm
+++ server/lib/NicToolServer/Nameserver.pm
@@ -35,6 +35,7 @@ SELECT ns.nt_nameserver_id, ns.nt_group_id, ns.name, ns.description, ns.address,
     if ($usable_string && scalar @usable) {
         $sql .= " OR ns.nt_nameserver_id IN ($usable_string)";
     };
+    $sql .= " ORDER BY ns.nt_nameserver_id";
     $sql .= ")";
 
     #warn $sql;
