2ffa954d51302b27c031beb6ededc965678d312d
Havard Eidnes <he@uninett.no>
2017-03-13 14:38:43 +0100

    The "critical" flag is only one byte, fix tinydns export accordingly.

--- server/lib/NicToolServer/Export/tinydns.pm
+++ server/lib/NicToolServer/Export/tinydns.pm
@@ -340,7 +340,7 @@ sub zr_caa {
     my $r = shift or die;
 
     # First flag byte
-    my $rdata = octal_escape( pack "n", $r->{weight} );
+    my $rdata = octal_escape( pack "C", $r->{weight} );
     # Then property tag as a length-prefixed text string
     $rdata .= $self->characterCount( $r->{other} ) .
 	$self->escape( $r->{other} );
