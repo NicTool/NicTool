000d8c7fcb2e088ce1ca5247d5af67ed0aa52b38
Matt Simerson <matt@tnpi.net>
2020-12-31 22:10:52 -0800

    travis: more test scripting

--- .test/setup-mysql.sh
+++ .test/setup-mysql.sh
@@ -1,22 +1,38 @@
 #!/bin/sh
 
+install_mysqld()
+{
+    sudo apt-get install -y mysql-server mysql-client
+}
+
+install_mysql_perl()
+{
+    sudo apt-get install -y libmysqlclient-dev libdbi-perl libdbd-mysql-perl libdbix-simple-perl
+    # sudo cpanm --notest DBD::mysql
+}
+
+configure_mysqld()
+{
+    echo 'sql_mode=""' | sudo tee -a /etc/mysql/mysql.conf.d/mysqld.cnf
+}
 
 if [ -n "$TRAVIS" ]; then
-    #echo 'sql_mode=""' | sudo tee -a /etc/mysql/mysql.conf.d/mysqld.cnf
+    #configure_mysqld
     #sudo service mysql restart
     mysql -u root -e 'SET GLOBAL sql_mode = "";'
 
 elif [ -n "$GITHUB_ACTIONS" ]; then
-    echo 'sql_mode=""' | sudo tee -a /etc/mysql/mysql.conf.d/mysqld.cnf
-    sudo apt-get install -y libmysqlclient-dev libdbi-perl libdbd-mysql-perl libdbix-simple-perl
-    # sudo cpanm --notest DBD::mysql
+
+    # install_mysql_perl
+    configure_mysqld
     sudo service mysql start
     mysql -u root -proot -e 'SET GLOBAL sql_mode = "";'
 
 else
-    echo 'sql_mode=""' | sudo tee -a /etc/mysql/mysql.conf.d/mysqld.cnf
-    sudo apt-get install -y mysql-server mysql-client libmysqlclient-dev libdbi-perl libdbd-mysql-perl libdbix-simple-perl
-    # sudo cpanm --notest DBD::mysql
+
+    # install_mysqld
+    # install_mysql_perl
+    configure_mysqld
     sudo service mysql start
     sudo mysql -e 'SET GLOBAL sql_mode = "";'
 
