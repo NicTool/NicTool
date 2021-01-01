4eb1e8de94ad16231b8acf6dd5df13713111dd8a
Matt Simerson <matt@tnpi.net>
2020-12-31 21:54:43 -0800

    travis: local perl modules

--- .test/setup-apache.sh
+++ .test/setup-apache.sh
@@ -2,63 +2,62 @@
 
 . .test/base.sh || exit
 
-for _f in apache.conf nictoolclient.conf nictoolserver.conf;
-do
-    if ! grep -q NT_HOME ".test/$_f"; then
-        echo "skip .test/$_f"
-        continue
+install_apache2()
+{
+    sudo apt-get install -y apache2 libapache2-mod-perl2 libapache2-mod-perl2-dev libapache-dbi-perl
+    sudo cpanm --notest Apache::DBI
+}
+
+config_modules()
+{
+    echo "enabling SSL module"
+    sudo a2enmod ssl
+
+    echo "switching to prefork"
+    sudo a2dismod mpm_event
+    sudo a2dismod mpm_worker
+    sudo a2enmod mpm_prefork
+}
+
+config_apache_site()
+{
+    for _f in apache.conf nictoolclient.conf nictoolserver.conf;
+    do
+        if ! grep -q NT_HOME ".test/$_f"; then
+            echo "skip .test/$_f"
+            continue
+        fi
+
+        echo "updating NT_HOME in .test/$_f"
+        if echo "$OSTYPE" | grep -q darwin; then
+            sed -i '' -e "s|NT_HOME|$NICTOOL_HOME|g" ".test/$_f"
+        else
+            sed -i -e "s|NT_HOME|$NICTOOL_HOME|g" ".test/$_f"
+        fi
+    done
+
+    echo "installing /etc/apache2/sites-enabled/nictool.conf"
+    sudo cp .test/apache.conf /etc/apache2/sites-enabled/nictool.conf
+
+    if [ -e "/etc/apache2/sites-enabled/000-default.conf" ]; then
+        echo "deleting 000-default.conf"
+        sudo rm /etc/apache2/sites-enabled/000-default.conf
     fi
-
-    echo "updating NT_HOME in .test/$_f"
-    if echo "$OSTYPE" | grep -q darwin; then
-        sed -i '' -e "s|NT_HOME|$NICTOOL_HOME|g" ".test/$_f"
-    else
-        sed -i -e "s|NT_HOME|$NICTOOL_HOME|g" ".test/$_f"
-    fi
-done
-
-if [ ! -f "client/lib/nictoolclient.conf" ]; then
-    cp .test/nictoolclient.conf client/lib/
-fi
-
-if [ ! -f "server/lib/nictoolserver.conf" ]; then
-    cp .test/nictoolserver.conf server/lib/
-fi
-
-echo "installing /etc/apache2/sites-enabled/nictool.conf"
-sudo cp .test/apache.conf /etc/apache2/sites-enabled/nictool.conf
-
-if [ -e "/etc/apache2/sites-enabled/000-default.conf" ]; then
-    echo "deleting 000-default.conf"
-    sudo rm /etc/apache2/sites-enabled/000-default.conf
-fi
+}
 
 if [ -n "$TRAVIS" ]; then
-    echo "enabling SSL module"
-    sudo a2enmod ssl
-    # sudo a2dismod mpm_event
-    # sudo a2dismod mpm_worker
-    # sudo a2enmod mpm_prefork
-    # sudo service apache2 restart || sudo cat /var/log/apache2/error.log
-elif [ -n "$GITHUB_ACTIONS" ]; then
-    sudo apt-get install -y apache2 libapache2-mod-perl2 libapache2-mod-perl2-dev libapache-dbi-perl
-    echo "enabling SSL module"
-    sudo a2enmod ssl
-    sudo a2dismod mpm_event
-    sudo a2dismod mpm_worker
-    sudo a2enmod mpm_prefork
-    sudo cpanm --notest Apache::DBI
-    sudo systemctl restart apache2 || sudo cat /var/log/apache2/error.log
-    sleep 3
 
+    echo "enabling SSL module"
+    sudo a2enmod ssl
+    config_apache_site
+    # config_modules
+    sudo service apache2 restart || sudo cat /var/log/apache2/error.log
+
+#elif [ -n "$GITHUB_ACTIONS" ]; then
 else
-    sudo apt-get install -y apache2 libapache2-mod-perl2 libapache2-mod-perl2-dev libapache-dbi-perl
-    echo "enabling SSL module"
-    sudo a2enmod ssl
-    sudo a2dismod mpm_event
-    sudo a2dismod mpm_worker
-    sudo a2enmod mpm_prefork
-    sudo cpanm --notest Apache::DBI
-    sudo systemctl restart apache2 || sudo cat /var/log/apache2/error.log
 
+    install_apache2
+    config_modules
+    config_apache_site
+    sudo systemctl restart apache2 || sudo cat /var/log/apache2/error.log
 fi
