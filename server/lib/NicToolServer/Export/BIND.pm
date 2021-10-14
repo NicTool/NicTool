f0c8b86d7ec61088278f57fac4ea97108a78aebb
Gerhard <gerhard@tinned-software.net>
2021-10-14 18:50:29 +0200

    Added sort order for exported bind include file (#270)

--- server/lib/NicToolServer/Export/BIND.pm
+++ server/lib/NicToolServer/Export/BIND.pm
@@ -36,7 +36,7 @@ sub update_named_include {
     # full export, write a new include  file
     my $datadir = $self->{nte}->get_export_data_dir || $dir;
     my $fh = $self->get_export_file( 'named.conf.nictool', $dir );
-    foreach my $zone ( $self->{nte}->zones_exported ) {
+    foreach my $zone ( sort $self->{nte}->zones_exported ) {
         my $tmpl = $self->get_template($dir, $zone);
         if ( $tmpl ) {
             print $fh $tmpl;
